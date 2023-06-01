1f6f287467b53cecfd2cabde2bd0b1e9cd2bbafe
Ripassorker <ripassorker02@glail.com>
2023-06-02 02:11:14 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 
-import cors from "cors";
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-   res.send("Server is running,,,,");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running,,,,')
+})
 
-export default app;
+export default app
