43b352d5cf3d3407cf75a84df6eb0673400a2ce2
Ripassorker <ripassorker02@glail.com>
2023-06-02 02:12:38 +0600

    done ,setup eslint , pretiier, huskey, lint stage

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Server is running,,,,')
+  res.send('Server is running on port 5000 ,,,,')
 })
 
 export default app
